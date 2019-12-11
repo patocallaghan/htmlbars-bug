2e53b72271ee7edd182b96a727570a97637ebb50
Pat O'Callaghan <pat@intercom.io>
2019-12-11 22:15:06 +0000

    Octanify the repo

--- .ember-cli.js
+++ .ember-cli.js
@@ -1,4 +1,9 @@
-{
+'use strict';
+const { setEdition } = require('@ember/edition-utils');
+
+setEdition('octane');
+
+module.exports = {
   /**
     Ember CLI sends analytics information by default. The data is completely
     anonymous, but there are times when you might want to disable this behavior.
@@ -6,4 +11,4 @@
     Setting `disableAnalytics` to true will prevent any data from being sent.
   */
   "disableAnalytics": false
-}
+};
